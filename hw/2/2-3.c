4b66f1e67dd9143b5432e26ffc70bf557cd14703
parkchanjong <7000cj@gmail.com>
2018-10-25 14:59:40 +0900

    hw complt

--- hw/2/2-3.c
+++ hw/2/2-3.c
@@ -9,40 +9,38 @@
 int main(void) {
 
 
-	int a, b, c;
+	int a = 0, b = 0, c = 0;
 
 
+	for (;;) {
 
-	printf("삼각형의 세변을  입력 : ");
-	scanf_s("%d %d %d", &a, &b, &c);
 
-	if (a < 0 || b < 0 || c < 0) {
-		printf("0(음수)가 입력되어 종료합니다.\n");
-		system("PAUSE");
-	}
-
-	while (1) {
 		printf("삼각형의 세변을  입력 : ");
 		scanf_s("%d %d %d", &a, &b, &c);
+
+
+		if (a < 0 || b < 0 || c < 0)
+			printf("0(음수)가 입력되어 종료합니다.\n");
+		break;
+
 		if (a + b <= c || a + c <= b || b + c <= a)
 			printf("삼각형을 만들수 없습니다\n");
 		continue;
+
+
+
+		if (a == b && b == c)
+			printf("정삼각형\n");
+		break;
+
+		if (a == b || a == c || b == c)
+			printf("이등변 삼각형\n");
+		break;
+
 		if (a + b > c || a + c > b || b + c > a)
-			break;
+			printf("일반삼각형\n");
+		break;
 	}
-
-
-
-	if (a == b && b == c)
-		printf("정삼각형\n");
-	else if (a == b || a == c || b == c)
-		printf("이등변 삼각형\n");
-	else
-		printf("일반삼각형\n");
-
-
-
-
 
 
 
